acf21dc09f11b4476c0216a77c5714c5cdf7ec2b
Tonderai Muchada <tonderaimuchada@gmail.com>
2018-12-17 13:16:10 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './../../index.html',
+  styleUrls: ['./../../css/style.css']
 })
 export class AppComponent {
   title = 'mary-mount';
